91b5b822cb825522a4baa8dd9dcbf4f083fd2c36
theetherGit <shivam@Shivams-Mac-mini.local>
2024-06-11 16:34:39 +0530

    Tried Implementation on the basis of vue-og-images

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -16,7 +16,26 @@ export interface ImageResponseOptions {
     format?: 'svg' | 'png'; // Defaults to 'png'
 }
 
-export type ComponentOptions = {
-    props?: Record<string, any>;
-    style?: string;
-};
+export interface RuntimeCompatibilitySchema {
+    ['css-inline']: 'node' | 'wasm' | 'wasm-fs' | false
+    resvg: 'node' | 'wasm' | 'wasm-fs' | false
+    satori: 'node' | 'wasm' | 'wasm-fs' | false
+    wasm?: WasmPluginOptions
+}
+
+interface WasmPluginOptions {
+    /**
+     * Directly import the `.wasm` files instead of bundling as base64 string.
+     *
+     * @default false
+     */
+    esmImport?: boolean;
+    /**
+     * Avoid using top level await and always use a proxy.
+     *
+     * Useful for compatibility with environments that don't support top level await.
+     *
+     * @default false
+     */
+    lazy?: boolean;
+}
