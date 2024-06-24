001563c008e4ce6a8dc8584e954651530aff60e9
theetherGit <shivam@Shivams-Mac-mini.local>
2024-06-24 20:41:07 +0530

    test vercel edge

--- src/routes/sc/+server.ts
+++ src/routes/sc/+server.ts
@@ -1,11 +1,11 @@
 import type {RequestHandler} from "@sveltejs/kit";
-import {ImageResponse} from "$lib";
-import OG from "./OG.svelte";
+import {ImageResponse} from "$lib/image-response.js";
+import OG from "../(components)/OG.svelte";
 
 export const GET: RequestHandler = async () => {
     return new ImageResponse(
         OG as any,
-        {debug: true},
-        { text: 'Ready to dive in?', spanText: 'Start your free trial today.' }
+        {debug: false, width: 800, height: 400},
+        { text: 'Ready to try sveltekit-og?', spanText: 'I know, took me 2 years to fix this library.' }
     );
 };
