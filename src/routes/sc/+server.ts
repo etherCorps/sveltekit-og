6394e4c6fb52cf07b360f710a980af0946f2cc38
theetherGit <shivam@Shivams-Mac-mini.local>
2024-06-24 20:28:14 +0530

    more clean up

--- src/routes/sc/+server.ts
+++ src/routes/sc/+server.ts
@@ -5,10 +5,7 @@ import OG from "./OG.svelte";
 export const GET: RequestHandler = async () => {
     return new ImageResponse(
         OG as any,
-        {debug: false},
-        { props: {
-                text: 'Ready to dive in?', spanText: 'Start your free trial today.'
-            }
-        }
+        {debug: true},
+        { text: 'Ready to dive in?', spanText: 'Start your free trial today.' }
     );
 };
