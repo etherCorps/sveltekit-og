3afd24ff9f26cbb96cc462308e32f13d00d16dd8
theetherGit <shivam@Shivams-Mac-mini.local>
2024-06-24 20:41:07 +0530

    test vercel edge

--- src/routes/edge/+server.ts
+++ src/routes/edge/+server.ts
@@ -1,19 +1,23 @@
-import { type RequestHandler } from '@sveltejs/kit';
+import { json, type RequestHandler } from '@sveltejs/kit';
 import {ImageResponse} from "$lib/image-response.js";
+import type { Config } from '@sveltejs/adapter-vercel';
 
+export const config: Config = {
+	runtime: 'edge',
+};
 
 export const GET: RequestHandler = async () => {
-    const html = `
+	const html = `
 <div tw="h-full w-full flex flex-col items-center justify-center bg-white text-sm font-bold">
   <img tw="w-40 h-40" src="https://www.ethercorps.io/logo_transparent.png"/>
   <div style="margin-top: 20px; color: gray">Hello, OGs</div>
   </div>
 `.replaceAll('\n', '').trim()
 
-    return new ImageResponse(html, {
-        format: 'png',
-        debug: false,
-        height: 300,
-        width: 600,
-    })
+	return new ImageResponse(html, {
+		format: 'png',
+		debug: false,
+		height: 300,
+		width: 600,
+	})
 };
---
+++ src/routes/edge/sc/+server.ts
@@ -0,0 +1,18 @@
+import type {RequestHandler} from "@sveltejs/kit";
+import {ImageResponse} from "$lib/image-response.js";
+import OG from "../../(components)/OG.svelte";
+import type { Config } from '@sveltejs/adapter-vercel';
+
+export const config: Config = {
+	runtime: 'edge',
+};
+
+
+
+export const GET: RequestHandler = async () => {
+	return new ImageResponse(
+		OG as any,
+		{debug: false, width: 800, height: 400},
+		{ text: 'Ready to try sveltekit-og?', spanText: 'I know, took me 2 years to fix this library.' }
+	);
+};
