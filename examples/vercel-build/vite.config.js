60b709b397cc63074d1151f909758879bd0716bf
theetherGit <meenashivam9650@gmail.com>
2025-06-05 03:55:50 +0530

    Vercel Test

--- examples/vercel-build/vite.config.js
+++ examples/vercel-build/vite.config.js
@@ -1,16 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import wasm from 'vite-plugin-wasm';
-import { vitePluginSvelteH2J } from '@ethercorps/svelte-h2j/vite';
-import { rollup as wasmPlugin } from 'unwasm/plugin';
 
 const config = {
-	plugins: [wasm(), vitePluginSvelteH2J(), sveltekit()],
+	plugins: [sveltekit()],
 	build: {
 		rollupOptions: {
-			plugins: [wasmPlugin({
-				esmImport: true,
-				lazy: true
-			})],
+			external: ['@ethercorps/sveltekit-og']
+			// plugins: [wasmPlugin({
+			// 	esmImport: true,
+			// 	lazy: true
+			// })],
 		}
 	}
 };
