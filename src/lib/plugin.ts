260b44b965de802d6e5d6783230d90392082141d
theetherGit <meenashivam9650@gmail.com>
2025-11-20 13:15:29 +0530

    Support for Node adapter

--- src/lib/plugin.ts
+++ src/lib/plugin.ts
@@ -1,10 +1,11 @@
 import { unwasm, type UnwasmPluginOptions } from "unwasm/plugin"
+import { isNode } from 'std-env';
 
 type Plugin = ReturnType<typeof unwasm>
 
 export function rollupWasm(options?: UnwasmPluginOptions): Plugin {
 	return unwasm({
-		esmImport: true,
+		esmImport: !isNode,
 		lazy: true,
 		...options
 	})
