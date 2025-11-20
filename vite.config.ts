52c73795b0c469e4fcff7229cf928fdcaf7e4351
theetherGit <meenashivam9650@gmail.com>
2025-11-20 12:42:10 +0530

    Fix mistake

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import wasm from "vite-plugin-wasm";
 import { visualizer } from 'rollup-plugin-visualizer';
 import { rollupWasm } from './src/lib/plugin.js';
 
