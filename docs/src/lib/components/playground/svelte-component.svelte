e683a3ffca5c35e5a750925a9a70d6f838f725f6
theetherGit <meenashivam9650@gmail.com>
2025-11-23 02:00:47 +0530

    docs(playground): Svelte Component example playground

---
+++ docs/src/lib/components/playground/svelte-component.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import {Button, Label, Input} from '@svecodocs/kit';
+
+	let githubDetails = $state({
+		owner: 'sveltejs',
+		repo: 'kit',
+	})
+
+	let imageSrc = $state(`/svelte?owner=sveltejs&repo=kit`)
+
+	function newOg() {
+		imageSrc = `/svelte?owner=${githubDetails.owner}&repo=${githubDetails.repo}`
+	}
+</script>
+
+<div class="flex flex-row items-center gap-x-5 w-full">
+	<div class="flex flex-col gap-y-3 w-full">
+		<Label>Github Username</Label>
+		<Input bind:value={githubDetails.owner}/>
+	</div>
+	<div class="flex flex-col gap-y-3 w-full">
+		<Label>User Repository</Label>
+		<Input bind:value={githubDetails.repo}/>
+	</div>
+	<div class="flex flex-col gap-y-3 w-full">
+		<Label class="truncate">Create for @{githubDetails.owner}/{githubDetails.repo}</Label>
+		<Button onclick={newOg} class="w-full">Generate</Button>
+	</div>
+</div>
+
+<div class="flex flex-col">
+	<img class="rounded-lg" src={imageSrc} alt="Playground" />
+	<span class="mt-2">Preview link:
+		<a class="italic text-rose-400" target="_blank" rel="noreferrer" referrerpolicy="no-referrer" href={imageSrc}>{imageSrc}</a></span>
+</div>
+
