75a0ef8c3b2c416a6fb0e09b2946f7c44bcab6ab
theetherGit <meenashivam9650@gmail.com>
2025-11-23 20:39:43 +0530

    feat(example): Netlify url import doesn't work

--- examples/netlify-build/src/lib/utils/helper.ts
+++ examples/netlify-build/src/lib/utils/helper.ts
@@ -1,11 +1,9 @@
 import { GoogleFont, CustomFont } from '@ethercorps/sveltekit-og/fonts';
-import { read } from '$app/server';
-import JetbrainsRegular from "./JetBrainsMono-Regular.ttf"
 
 const jetbrainsMonoBoldURL = 'https://github.com/JetBrains/JetBrainsMono/raw/master/fonts/ttf/JetBrainsMono-Bold.ttf'
 
 export const fonts = [
-	new CustomFont('JetBrains Mono', () => read(JetbrainsRegular).arrayBuffer(), {weight: 400}),
 	new GoogleFont('JetBrains Mono', { weight: 500 }),
+	new GoogleFont('JetBrains Mono', { weight: 400 }),
 	new CustomFont('JetBrains Mono', () => fetch(jetbrainsMonoBoldURL).then((res) => res.arrayBuffer()), { weight: 700 }),
 ];
