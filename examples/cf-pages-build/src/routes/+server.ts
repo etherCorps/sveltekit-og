a26c54b0778fd68f64081110586fbe8470c27759
theetherGit <meenashivam9650@gmail.com>
2025-11-23 19:48:30 +0530

    feat(docs): fix design

--- examples/cf-pages-build/src/routes/+server.ts
+++ examples/cf-pages-build/src/routes/+server.ts
@@ -5,18 +5,18 @@ import { GoogleFont, CustomFont, resolveFonts } from '@ethercorps/sveltekit-og/f
 import { read } from '$app/server';
 
 const template = `
- <div tw="bg-gray-50 flex w-full h-full items-center justify-center">
-    <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-      <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
+ <div class="bg-gray-50 flex flex-col w-full h-full justify-center">
+    <div class="flex flex-col w-full justify-between p-8">
+      <h2 class="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
         <span>Ready to dive in?</span>
-        <span tw="text-indigo-600">Start your free trial today.</span>
+        <span class="text-indigo-600">Start your free trial today.</span>
       </h2>
-      <div tw="mt-8 flex md:mt-0">
-        <div tw="flex rounded-md shadow">
-          <a href="#" tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white">Get started</a>
+      <div class="flex flex-row items-start">
+        <div class="flex rounded-md shadow">
+          <button class="flex justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white">Get started</button>
         </div>
-        <div tw="ml-3 flex rounded-md shadow">
-          <a href="#" tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600">Learn more</a>
+        <div class="ml-3 flex rounded-md shadow">
+          <button class="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600">Learn more</button>
         </div>
       </div>
     </div>
