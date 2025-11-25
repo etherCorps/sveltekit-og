96d786bfaf25933f10d53d18bb1b2c05b62c382b
theetherGit <meenashivam9650@gmail.com>
2025-11-25 09:44:56 +0530

    feat(package): Fix vercel pre rendering

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,27 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleUnseenRoutes: (details) => {
+				console.log('handleUnseenRoutes', JSON.stringify(details, null, 2));
+				return;
+			},
+			handleHttpError: ({ path, message }) => {
+				console.log('handleHttpError', path, message);
+				// ignore deliberate link to shiny 404 page
+				if (path.split('/').length === 2) return;
+
+				if (path.startsWith('/og.png')) return;
+
+				// otherwise fail the build
+				throw new Error(message);
+			},
+			handleMissingId: (details) => {
+				console.log('handleMissingId', JSON.stringify(details, null, 2));
+				return;
+			}
+		}
 	}
 };
 
