4d2fa095063198c72a7f82d202b25653ac245fb7
theetherGit <meenashivam9650@gmail.com>
2025-12-02 17:46:03 +0530

    chore(docs): Pre render og image

--- docs/src/routes/(og)/og.png/+server.ts
+++ docs/src/routes/(og)/og.png/+server.ts
@@ -2,6 +2,8 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { ImageResponse } from '@ethercorps/sveltekit-og';
 import OG from './OG.svelte';
 
+export const prerender = true;
+
 export const GET: RequestHandler = async () => {
 	return new ImageResponse(OG, {
 		width: 1200,
