b7b02a08e113873d1cd31c5fafd1edc355b9c8d1
theetherGit <meenashivam9650@gmail.com>
2025-12-02 17:29:43 +0530

    feat(docs): Play a bit with default og image

--- docs/src/routes/(og)/og.png/OG.svelte
+++ docs/src/routes/(og)/og.png/OG.svelte
@@ -1,20 +1,20 @@
 <svelte:options css="injected" />
 
 <script lang="ts">
-	import Logo from '../(assets)/ethercorps-logo.png?inline';
 	import SvelteLogo from '../(assets)/svelte-logo.svg?raw';
+	import Logo from './logo-icon.svelte';
 </script>
 
 <div class="flex flex-col bg-gradient w-full h-full p-5">
 	<div class="flex flex-row justify-normal items-center">
-		<img src={Logo} class="w-8" />
-		<span class="ml-2 font-semibold text-xl text-gray-100">@ethercorps/sveltekit-og</span>
+		<Logo class="w-5 h-5" startColor="#4f46e5" endColor="#a5b4fc"/>
+		<span class="ml-2 font-semibold text-xl text-orange-100">@ethercorps/sveltekit-og</span>
 	</div>
 	<div class="flex flex-row items-center justify-between w-full h-[90%] px-10">
 		<div class="flex flex-col justify-center h-full w-1/2">
-			<p class="text-lg -mb-3 text-teal-200 font-bold tracking-wider">SVELTEKIT OG</p>
-			<p class="text-5xl font-bold text-teal-400 tracking-widest">VERSION 4 ✨</p>
-			<p class="-mt-2 text-lg text-gray-300">
+			<p class="text-lg -mb-3 text-indigo-100 font-bold tracking-wider">SVELTEKIT OG</p>
+			<p class="text-5xl font-bold text-indigo-200 tracking-widest">VERSION 4 ✨</p>
+			<p class="-mt-2 text-lg text-indigo-300 opacity-65">
 				Sveltekit OG v4 with support for Node, Deno, Cloudflare Workers, Pages, Vercel and Netlify
 			</p>
 		</div>
@@ -22,16 +22,16 @@
 			{@html SvelteLogo}
 		</div>
 	</div>
-	<div class="flex flex-row text-gray-400">sveltekit-og.dev</div>
+	<div class="flex flex-row text-gray-50">sveltekit-og.dev</div>
 </div>
 
 <style>
 	.bg-gradient {
 		background: linear-gradient(
 			to right,
-			#2c5364,
-			#203a43,
-			#0f2027
+      #3730a3,
+      #4f46e5,
+      #4f46e5
 		); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 	}
 </style>
